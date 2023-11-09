ab0c40c6fee0c5cc98512b7ef609394e81994049
Stark <mattstark08@gmail.com>
2023-11-08 19:15:26 -0600

    set up notes, and beginning to work on the schema

---
+++ 02-Challenge/db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+CREATE DATABASE employeetracker_db;
+
+
+USE employeetracker_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
