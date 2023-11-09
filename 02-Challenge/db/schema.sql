e07646ccf1e160b3699f734188823a2f9818b6d2
Stark <mattstark08@gmail.com>
2023-11-08 19:40:43 -0600

    working on schema

--- 02-Challenge/db/schema.sql
+++ 02-Challenge/db/schema.sql
@@ -25,10 +25,5 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-
   role_id INT,
-  manager_id INT REFERENCES employee(id),
-  FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL
 );
